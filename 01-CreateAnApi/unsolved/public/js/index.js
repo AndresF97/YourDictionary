fe9954328f13aca1efb7c416b9b6d7f51559c23a
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-03-14 16:57:44 -0700

    added an alert that will show up dynamically

--- 01-CreateAnApi/unsolved/public/js/index.js
+++ 01-CreateAnApi/unsolved/public/js/index.js
@@ -1,8 +1,7 @@
+$('.alert').alert("close") 
 
-$("#searchWords").on("click",function(event){
-    $("#wordsContainer").empty()
+function getWords(){
     var words = [];
-    event.preventDefault()
     $.ajax({
         method:"GET",
         url:"/api/all"
@@ -13,19 +12,25 @@ $("#searchWords").on("click",function(event){
         $("#wordsContainer").append(`
         <form data-word="${result[i].id}">
         <div class="rounded border border-primary m-4">
-        <div class="m-3">
-            <h3>${(result[i].WORD).toUpperCase()}:</h3>
-            <textarea readonly>
-                ${result[i].MEANING}
-            </textarea>
-            <button id="deleteBtn" data-delete="${result[i].id}" type="button"class="btn btn-danger">Delete</button>
-            <button id="updateBtn" data-update="${result[i].id}" type="button" class="btn btn-primary">Update</button> 
-        </div>
+            <div class="m-3">
+                <h3>${(result[i].WORD).toUpperCase()}:</h3>
+                <textarea readonly>
+                    ${result[i].MEANING}
+                </textarea>
+            </div>
+        <div class="text-right m-2">
+        <button id="deleteBtn" data-delete="${result[i].id}" type="button"class="btn btn-danger">Delete</button>
+        <button id="updateBtn" data-update="${result[i].id}" type="button" class="btn btn-primary">Update</button>
         </div>
         </form>
         `)
         }
     })
+}
+$("#searchWords").on("click",function(event){
+    event.preventDefault()
+    $("#wordsContainer").empty()
+    getWords()
 })
 
 $(document).on("click","#deleteBtn", function(event){
@@ -59,19 +64,38 @@ $(document).on("click","#create",function(event){
     var fullDate = year+"-"+month+"-"+day
     var word= ($("#word").val().trim())
     var meaning = ($("#meaning").val().trim())
-    var createdNewWords = {
-        WORD:word,
-        MEANING:meaning,
-        createdAt:fullDate,
-        updatedAt:fullDate
+    if(word === "" && meaning === ""){
+        console.log("alert")
+        $("#alertArea").append(`
+        <div class="alert alert-warning alert-dismissible fade show m-2" role="alert">
+            <strong>Please Enter A word</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>`
+        )
+    }else{
+        var createdNewWords = {
+            WORD:word,
+            MEANING:meaning,
+            createdAt:fullDate,
+            updatedAt:fullDate
+        }
+        console.log(createdNewWords)
+        $.ajax({
+            type:"PUT",
+            url:"/api/word",
+            data:createdNewWords
+        })
+        .then(function(){
+            location.reload()
+            .then(function(){
+                console.log("here")
+                $(document).ready(function(){
+                    console.log("runs")
+                    getWords()
+                })
+            })
+        })
     }
-    console.log(createdNewWords)
-    $.ajax({
-        type:"PUT",
-        url:"/api/word",
-        data:createdNewWords
-    })
-    .then(function(){
-        location.reload()
-    })
 })
